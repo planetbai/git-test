e8e1d1c2ca78db759f6edb9274736ebb5bed0a11
baihongna <baihongna@hzyushi.cn>
2022-07-17 01:04:33 +0800

    second commit

--- src/main/java/com/bai/git/Git.java
+++ src/main/java/com/bai/git/Git.java
@@ -7,5 +7,6 @@ package com.bai.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
