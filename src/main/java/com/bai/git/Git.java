f29f7f26de2690a735125cfaa5f2694fee7d1f61
baihongna <baihongna@hzyushi.cn>
2022-07-17 01:01:30 +0800

    first commit

---
+++ src/main/java/com/bai/git/Git.java
@@ -0,0 +1,11 @@
+package com.bai.git;
+
+/**
+ * @author wuya
+ * @date 2022/7/17
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
